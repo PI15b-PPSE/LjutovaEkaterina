bbd04cd879c0a404fffc2285cd5a5a9b3acab1d7
Екатерина Лютова <34005740+fuksy@users.noreply.github.com>
2018-03-06 20:09:37 +1400

    Добавлена функция случайного расположения бомбы в ячейках.

--- javascript/sapper.js
+++ javascript/sapper.js
@@ -1,1 +1,16 @@
-
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    // Случайное число для колонки от 0 до кол-ва столбцов -1
+    function getRandomColumnIndex() {
+       return getRandomInt(0, columnsCount - 1);
+    }
+    // Случайное число для строки от 0 до кол-ва строк -1
+    function getRandomRowIndex() {
+        return getRandomInt(0, rowsCount - 1);
+    }
+    // Создать бомбу со случайными координатами
+    function getBomb() {
+        return [getRandomRowIndex(), getRandomColumnIndex()];
+    }
+}
