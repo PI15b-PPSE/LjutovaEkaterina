5ec71cbc564ad9c3b066c28307fe5ae4d12e30a6
Екатерина Лютова <34005740+fuksy@users.noreply.github.com>
2018-04-16 19:29:49 +0300

    Добавление функции изменения размера минного поля

--- javascript/sapper.js
+++ javascript/sapper.js
@@ -1,3 +1,21 @@
+function changeSizeHandler(e) {
+    e.preventDefault();
+    var button = $(this);
+    var attrs = ["data-rows-count", "data-columns-count", "data-bomb-count"];
+    for (var i = 0; i < attrs.length; i++) {
+        $("table.sapper-table:first").attr(attrs[i], button.attr(attrs[i]));
+}
+    var columns = parseInt(button.attr("data-columns-count"));
+    var width = columns * 30 + 2;
+    $("div.panel.sapper-table:first").width(width);
+    loadGame();
+function loadGame() {
+    $("div.panel.sapper-table:first>.panel-heading>.panel-title span.bombs-refresh").remove();
+    $("div.panel.sapper-table:first>.panel-heading>.panel-title span.bombs-count-icon").remove();
+    $("div.panel.sapper-table:first>.panel-heading>.panel-title span.timer").remove();
+    $("div.panel.sapper-table:first>.panel-heading>.panel-title span.bombs-count").remove();
+
+}
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
