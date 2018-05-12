a4dafa16e145c65cba906c771d2410fe09013d95
fuksy <ekaterina_ljutova1998@rambler.ru>
2018-05-12 23:59:14 +0300

    Изменение расстояния между ячейками

--- javascript/sapper.js
+++ javascript/sapper.js
@@ -10,7 +10,7 @@ function changeSizeHandler(e) {
         $("table.sapper-table:first").attr(attrs[i], button.attr(attrs[i]));
     }
     var columns = parseInt(button.attr("data-columns-count"));
-    var width = columns * 30 + 2;
+    var width = columns * 20 + 2;
     $("div.panel.sapper-table:first").width(width);
     loadGame();
 }
@@ -20,6 +20,19 @@ function loadGame() {
     $("div.panel.sapper-table:first>.panel-heading>.panel-title span.bombs-count-icon").remove();
     $("div.panel.sapper-table:first>.panel-heading>.panel-title span.timer").remove();
     $("div.panel.sapper-table:first>.panel-heading>.panel-title span.bombs-count").remove();
+	 // количество строк
+    var rowsCount = parseInt($("table.sapper-table").attr("data-rows-count"));
+    // количество колонок
+    var columnsCount = parseInt($("table.sapper-table").attr("data-columns-count"));
+    // количество бомб
+    var bombCount = parseInt($("table.sapper-table").attr("data-bomb-count"));
+    // бомбы
+    var bombs = [];
+    // секунды таймера
+    var seconds = 0;
+    // игра окончена
+    var gameOver = false;
+    // использование Math.round() даст неравномерное распределение!
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -51,4 +64,49 @@ function getCell(ri, ci) {
      var cell = $(row).find("td > div")[ci];
      return $(cell);
 }
+// проверка ячейки, что она ещё не открыта
+    function checkCell(cell) {
+        return cell.hasClass("cell-closed") && cell.attr("data-flag") === undefined;
+    }
+    // установить окончание игры
+    function setGameOver() {
+        gameOver = true;
+        var table = $("table.sapper-table:first");
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        td.setAttribute("colspan", columnsCount);
+        td.setAttribute("class", "game-over");
+        td.innerHTML = "Game Over";
+        tr.appendChild(td);
+        table.append(tr);
+    }
+	// проверить ячейки вокруг ячейки с указанными координатами
+    function lookAround(ri, ci, funct) {
+        var beginRowIndex = ri === 0 ? 0 : ri - 1;
+        var endRowIndex = ri === rowsCount - 1 ? rowsCount - 1 : ri + 1;
+        var beginColumnIndex = ci === 0 ? 0 : ci - 1;
+        var endColumnIndex = ci === columnsCount - 1 ? columnsCount - 1 : ci + 1;
+        for (var bri = beginRowIndex; bri <= endRowIndex; bri++) {
+            for (var bci = beginColumnIndex; bci <= endColumnIndex; bci++) {
+                if (ri === bri && ci === bci) {
+                    continue;
+                }
+                funct(bri, bci);
+            }
+        }
+    }
+    // очистить ячейки вокруг с проверкой вокруг пустых ячеек
+    function clearAround(cell, ri, ci) {
+        lookAround(ri, ci, function (ri2, ci2) {
+            var cell2 = getCell(ri2, ci2);
+            if (checkCell(cell2)) {
+                clearCell(cell2);
+                if (countBombsAround(ri2, ci2) === 0) {
+                    clearAround(cell2, ri2, ci2);
+                } else {
+                    drawNumber(cell2, ri2, ci2);
+                }
+            }
+        });
+    }
 }
